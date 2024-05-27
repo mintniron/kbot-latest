63073d7c4042bd9afcfa7361bd380b5e0a102e63
Vlodko Sikach <qttdev004@gmail.com>
2024-05-27 17:46:52 +0300

    merge_files

--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 /*
-Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/vit-um/kbot/cmd"
+import "github.com/mintniron/kbot/cmd"
 
 func main() {
 	cmd.Execute()
